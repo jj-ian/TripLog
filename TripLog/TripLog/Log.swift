fa119a0c307d12663dcbd10143fe3578770035ef
Julie <julie.y.chien@gmail.com>
2016-03-13 22:57:58 -0400

    getting logs set up

---
+++ TripLog/TripLog/Log.swift
@@ -0,0 +1,28 @@
+//
+//  Log.swift
+//  TripLog
+//
+//  Created by Julie Chien on 3/13/16.
+//  Copyright © 2016 Julie Chien. All rights reserved.
+//
+
+import UIKit
+
+class Log: NSObject {
+    var startAddress: String
+    var endAddress: String
+    var startTime: NSDate
+    var endTime: NSDate
+    var timeElapsed: NSTimeInterval
+    
+    init(startAddress: String, endAddress: String, startTime: NSDate, endTime: NSDate, timeElapsed: NSTimeInterval) {
+        self.startAddress = startAddress
+        self.endAddress = endAddress
+        self.startTime = startTime
+        self.endTime = endTime
+        self.timeElapsed = timeElapsed
+        
+        super.init()
+    }
+
+}
---
+++ TripLog/TripLog/Model/LogStore.swift
@@ -0,0 +1,10 @@
+//
+//  LogStore.swift
+//  TripLog
+//
+//  Created by Julie Chien on 3/13/16.
+//  Copyright © 2016 Julie Chien. All rights reserved.
+//
+
+//import UIKit
+
